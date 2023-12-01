011ee31592b6abc3079eb26bab994ae10c41c5d4
ymh <hanyongming@gmail.com>
2023-12-01 16:28:14 +0800

    wip: stuck on module build step

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
+import './App.css';
 import {
   materialRenderers,
   materialCells,
 } from '@jsonforms/material-renderers';
 import { JsonForms } from '@jsonforms/react';
-
 import schema from './formtypes/schema.json';
-import './App.css';
+import $RefParser from "@apidevtools/json-schema-ref-parser";
 
 const initialData = {}
 
+
+
 function App() {
+  try {
+    const derefedSchema = $RefParser.dereference(schema);
+    console.log(derefedSchema);
+  }
+  catch(err) {
+    console.log(err);
+  }
+  
   const [data, setData] = useState(initialData);
   return (
     <div className="App">
       <JsonForms
         schema={schema}
-        // uischema={uischema}
         data={data}
         renderers={materialRenderers}
         cells={materialCells}
